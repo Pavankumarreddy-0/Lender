ed80422cc044900eeb3d091666931ffed071d840
PilogSample <pilogsample@gmail.com>
2023-04-03 17:11:20 +0530

    DEVBETA-1.01

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,6 +1,8 @@
 import { loginRoute } from "./loginRoute.js"
 import { signUpRoute } from "./signUpRoute.js"
+import { fetchRoles } from "./roles/fetchRoles.js"
 export const routes = [
     loginRoute,
     signUpRoute,
+    fetchRoles
 ]
