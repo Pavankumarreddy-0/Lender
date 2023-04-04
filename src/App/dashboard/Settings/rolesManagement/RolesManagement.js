daa269e164b6a5112d1a831d28452da9e0b5ddd0
kranthikiran333 <battukranthikiran@gmail.com>
2023-04-04 14:07:01 +0530

    updated

--- src/App/dashboard/Settings/rolesManagement/RolesManagement.js
+++ src/App/dashboard/Settings/rolesManagement/RolesManagement.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import axios from 'axios';
 
 export default function RolesManagement() {
+
+  const [roleManagement, setRoleManagement] = useState([]);
+
+  useEffect(()=>{
+      loadRoles();
+  },[]);
+
+
+  const loadRoles = async () =>{
+      console.log(localStorage.getItem("token"));
+      await axios.post('/api/roles/', { },{
+          headers: { authorization: `Bearer ${ localStorage.getItem("token") }` }
+      }).then(response => {
+
+          const { result } = response.data;
+          console.log(result);
+          setRoleManagement(result)
+
+      }).catch(error => {
+          console.error(error);
+  })
+  }
+
   return (
     <div>
-      Roles Management
+      {
+        roleManagement.map(e=>{
+          return <li>{e.roleName}</li>
+        })
+      }
     </div>
   )
 }
