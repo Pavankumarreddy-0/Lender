f7f3e16866e9f242885d56b8453d0053b192b726
kranthikiran333 <battukranthikiran@gmail.com>
2023-04-05 18:25:08 +0530

    DEVBETA-v1.25: Sidebar Module

--- src/App/dashboard/components/Sidebar/Sidebar.js
+++ src/App/dashboard/components/Sidebar/Sidebar.js
@@ -1,25 +1,64 @@
-import React from 'react'
+import sidebarStyles from './sidebar.module.css';
+import React, {useEffect, useState} from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Sidebar() {
-  return (
-    <div>
+  const sbItems ={
+    menuitems:[
+      {
+        url: "/dashboard",
+        icon:"bi bi-house",
+        name: "Home"
+      },
+      {
+        url: "/dashboard",
+        icon:"bi bi-columns",
+        name: "Platform"
+      },
+      {
+        url: "/dashboard",
+        icon:"bi bi-stack",
+        name: "Crowdfunding"
+      },
+      {
+        url: "/dashboard",
+        icon:"bi bi-people",
+        name: "Community"
+      },
+      {
+        url: "/dashboard",
+        icon:"bi bi-cpu-fill",
+        name: "Everything"
+      },
+      {
+        url: "/dashboard",
+        icon:"bi bi-columns-gap",
+        name: "Investments"
+      },
+      {
+        url: "/dashboard",
+        icon:"bi bi-cpu-fill",
+        name: "Everything"
+      },
+      
+    ]
+  }
+  const [menuitems,setmenuitems] = useState(sbItems);
+ 
 
-      <div>
-       <i class="bi bi-house"></i>
-       <br/>
-       <span>Home</span>
-       </div>
 
-       <div>
-       <i class="bi bi-columns"></i>
-       <br/>
-       <span>Platform</span>
-       </div>
-
-
-
-
-
-    </div>
+  return(
+    <>
+      <div className={sidebarStyles['iconsDiv']}>
+        {
+          menuitems.menuitems.map((e)=>{
+            return  <li className={sidebarStyles['iconsList']}> <i className={e.icon}></i><span>{e.name}</span> </li>
+          })
+        }
+      </div>
+    
+    </>
   )
+    
+  
 }
